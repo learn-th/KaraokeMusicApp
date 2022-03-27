4c58f1068f2aebe89493c2e2e1ea04355a542d8b
Thiago Aragão <thiago.aragao@worthix.com>
2022-03-27 16:15:58 -0300

    correçao do package.json

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/karaoke-do-th'));
+app.use(express.static('./dist/KaraokeMusicList'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/karaoke-do-th/'}),
+    res.sendFile('index.html', {root: 'dist/KaraokeMusicList/'}),
 );
 
 // Start the app by listening on the default Heroku port
